a47dc225c6108f051350f035af228018eaf2b983
christineccy01 <christineccy01@gmail.com>
2025-08-31 20:30:03 +0800

    Build fix

--- app/stis/prevalence/page.tsx
+++ app/stis/prevalence/page.tsx
@@ -2,10 +2,6 @@ import { PrevalenceSection } from '../../components/PrevalenceSection';
 
 export default function STIPrevalencePage() {
   return (
-    <PrevalenceSection 
-      showNavigation={true}
-      showCallToAction={true}
-      isFullPage={true}
-    />
+    <PrevalenceSection />
   );
 }
