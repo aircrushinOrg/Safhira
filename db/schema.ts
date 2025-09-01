9a18d0a96f615043b210d93c84e233a8020311b6
Jay Yang <jsoo0009@student.monash.edu>
2025-09-01 14:35:48 +0800

    Inputted the csv datasource and create the STIstate table schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, serial, varchar, timestamp, text, boolean } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar, timestamp, text, boolean, integer, decimal, primaryKey, index, date, numeric } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -16,3 +16,18 @@ export const sessions = pgTable('sessions', {
   createdAt: timestamp('created_at').defaultNow().notNull(),
   expiresAt: timestamp('expires_at'),
 });
+
+// Table Creation
+// Sti state Table - Table recording the each STI incidence rate for Malaysia State
+export const stiState = pgTable('sti_state', {
+  date: integer('date').notNull(),
+  state: varchar('state', { length: 255 }).notNull(),
+  disease: varchar('disease', { length: 255 }).notNull(),
+  cases: integer('cases').notNull(),
+  incidence: numeric('incidence', { precision: 10, scale: 2 }).notNull(),
+}, (table) => [
+      primaryKey({ columns: [table.date, table.state, table.disease] }),        
+      index('idx_std_state_date').on(table.date),
+      index('idx_std_state_state').on(table.state),
+      index('idx_std_state_disease').on(table.disease),
+]);
